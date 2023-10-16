64688a3d5e2fba5f4ed64b57d84a466265fb5bc3
AnnaLindroos <alindroos98@gmail.com>
2023-10-16 14:46:35 +0200

    Implementering generiska metoder och arv

--- Car Rental Assignment/Program.cs
+++ Car Rental Assignment/Program.cs
@@ -11,6 +11,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-builder.Services.AddSingleton(bp => new BookingProcessor(new CollectionData()));
+builder.Services.AddSingleton<CollectionData>();
 
 await builder.Build().RunAsync();
